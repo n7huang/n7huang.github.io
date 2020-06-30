98d7aea93487a5df82569727df26d857bf12668f
Nicholas <huang.nick9@gmail.com>
2020-06-30 03:28:01 -0400

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-## Personal Website
-This project was bootstrapped with create-react-app, written in React with Aphrodite for CSS-in-JS.
-
-Current considerations:
-- generalize Timeline and InfoCards components further by uplifting colour/style props.
-- adding more sections: Interests, Current Hobbies, etc.
---
+++ precache-manifest.1b2537374f64bf9d5a6f6e069920a1f1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f5b036a8e27c08f05eed7f38efe61e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43774f5caeacd283d628",
+    "url": "/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "0d036c614d3f694061ea",
+    "url": "/static/css/main.4ea3dbcc.chunk.css"
+  },
+  {
+    "revision": "43774f5caeacd283d628",
+    "url": "/static/js/2.62e8a558.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.62e8a558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d036c614d3f694061ea",
+    "url": "/static/js/main.ac2b2b8a.chunk.js"
+  },
+  {
+    "revision": "dcac4cc8d4ff6459583c",
+    "url": "/static/js/runtime-main.ebf86c16.js"
+  },
+  {
+    "revision": "cbd3e7b137a81dfac7e7da21a72b6a8b",
+    "url": "/static/media/background_blurb.cbd3e7b1.png"
+  },
+  {
+    "revision": "850f4264baeca28d3e055b8cc9d76315",
+    "url": "/static/media/background_exp.850f4264.png"
+  },
+  {
+    "revision": "2ce79207273154fd5f7b6a15b710b939",
+    "url": "/static/media/background_skills.2ce79207.png"
+  },
+  {
+    "revision": "16b7e0ec06566e9845584c616dd568a3",
+    "url": "/static/media/ch.16b7e0ec.svg"
+  },
+  {
+    "revision": "ddd41c52119a72bcdadda43d71ea5ae5",
+    "url": "/static/media/jently.ddd41c52.svg"
+  },
+  {
+    "revision": "e1ef2ee8314a111d9e7aba5261101af0",
+    "url": "/static/media/ontario.e1ef2ee8.svg"
+  },
+  {
+    "revision": "9447ce2efbfada2791a98c4fe568f874",
+    "url": "/static/media/us.9447ce2e.svg"
+  },
+  {
+    "revision": "b8f4cacbb8381659898a61857d469901",
+    "url": "/static/media/uw.b8f4cacb.svg"
+  },
+  {
+    "revision": "594cf6a22a4bbd5b7643be339c951586",
+    "url": "/static/media/veeva.594cf6a2.svg"
+  },
+  {
+    "revision": "382f754e1da4c957b300f729a7e3fc83",
+    "url": "/static/media/wish.382f754e.svg"
+  }
+]);
