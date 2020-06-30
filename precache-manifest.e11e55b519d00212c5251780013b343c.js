e572f0ff698de6343efca88ecebf7989d1397544
Nicholas <huang.nick9@gmail.com>
2020-06-30 00:06:08 -0400

    Cleanup

---
+++ precache-manifest.e11e55b519d00212c5251780013b343c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02304eefec0d63fd4f122d10add84c8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65cb56b3d63082e4532b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "126aeff4082102611e0d",
+    "url": "/static/js/2.16f1aeb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16f1aeb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65cb56b3d63082e4532b",
+    "url": "/static/js/main.c1f31ab1.chunk.js"
+  },
+  {
+    "revision": "dcac4cc8d4ff6459583c",
+    "url": "/static/js/runtime-main.ebf86c16.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
